5a67abd1506e1ac92336e79357b5037d1fef35a3
Miurg <rashnimorgen@gmail.com>
2025-04-06 02:46:35 +0300

    Add auth queries unit tests

---
+++ BackendForChatTests/Queries/Auth/LoginTests.cs
@@ -0,0 +1,112 @@
+﻿using BackendForChat.Application.DTO.Requests;
+using BackendForChat.Application.Interfaces;
+using BackendForChat.Application.Queries.Auth;
+using BackendForChat.Application.Queries.Users;
+using BackendForChat.Application.Services;
+using BackendForChat.Models.DatabaseContext;
+using BackendForChat.Models.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BackendForChatTests.Queries.Auth
+{
+    [TestFixture]
+    public class LoginTests
+    {
+        private ApplicationDbContext _context;
+        private Mock<IPasswordHasher<UserModel>> _passwordHasherMock;
+        private Mock<IJwtService> _jwtServiceMock;
+        private LoginHandler _handler;
+
+        [SetUp]
+        public void SetUp()
+        {
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+            _context = new ApplicationDbContext(options);
+
+            _context.Users.Add(new UserModel { Username = "test_user", PasswordHash = "pass" });
+            _context.SaveChanges();
+
+            _passwordHasherMock = new Mock<IPasswordHasher<UserModel>>();
+            _jwtServiceMock = new Mock<IJwtService>();
+
+            _handler = new LoginHandler(_context, _passwordHasherMock.Object, _jwtServiceMock.Object);
+        }
+
+        [Test]
+        public async Task Login_ShouldReturn_SuccessResultAndToken_WhenCredentialsAreValid()
+        {
+            var user = await _context.Users.FirstAsync();
+            _passwordHasherMock
+                .Setup(h => h.VerifyHashedPassword(user, user.PasswordHash, "correct_password"))
+                .Returns(PasswordVerificationResult.Success);
+
+            _jwtServiceMock
+                .Setup(j => j.GenerateJwtToken(user))
+                .Returns("test_token");
+            var requestLogin = new RequestLoginDto
+            {
+                Username = "test_user",
+                Password = "correct_password"
+            };
+            var query = new LoginQuery(requestLogin);
+
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            Assert.That(result.Success, Is.True);
+            Assert.That(result.Data.Token, Is.EqualTo("test_token"));
+        }
+
+        [Test]
+        public async Task Login_ShouldReturn_FailResultAndErorrMessage_WhenUserDoesNotExist()
+        {
+            var requestLogin = new RequestLoginDto
+            {
+                Username = "not_exist",
+                Password = "any"
+            };
+            var query = new LoginQuery(requestLogin);
+
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            Assert.That(result.Success, Is.False);
+            Assert.That(result.ErrorMessage, Is.EqualTo("Invalid credentials"));
+        }
+
+        [Test]
+        public async Task Login_ShouldReturn_FailResultAndErrorMessage_WhenPasswordIsIncorrect()
+        {
+            var user = await _context.Users.FirstAsync();
+            _passwordHasherMock
+                .Setup(h => h.VerifyHashedPassword(user, user.PasswordHash, "wrong_password"))
+                .Returns(PasswordVerificationResult.Failed);
+
+            var requestLogin = new RequestLoginDto
+            {
+                Username = "test_user",
+                Password = "wrong_password"
+            };
+            var query = new LoginQuery(requestLogin);
+
+            var result = await _handler.Handle(query, CancellationToken.None);
+
+            Assert.That(result.Success, Is.False);
+            Assert.That(result.ErrorMessage, Is.EqualTo("Invalid credentials"));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            _context.Database.EnsureDeleted();
+            _context.Dispose();
+        }
+    }
+}
